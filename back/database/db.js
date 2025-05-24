fa73a6c87353466ae7d2caef48e668f84730dbdf
Aashish518 <jadavaashish9@gmail.com.com>
2025-05-24 18:44:04 +0530

    a

--- back/database/db.js
+++ back/database/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const mongoosea = () => {
-    mongoose.connect('mongodb://localhost:27017/test')
+// mongoose.connect('mongodb://localhost:27017/test')
+    mongoose.connect('mongodb+srv://jadavaashish9:An%40211518@cluster0.gtxxzpc.mongodb.net/todo?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log('Connected!'))
     .catch((e) => console.log(e));
 }
